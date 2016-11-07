279adeb6a3fc2266c2306fd0ceba58b1e868fbf1
SinhHa <sinh.ha1992@gmail.com>
2016-11-07 15:39:56 +0700

    commit showing tab layout

---
+++ app/src/main/java/com/example/dangtuanvn/movie_app/MovieTabFragment.java
@@ -0,0 +1,72 @@
+package com.example.dangtuanvn.movie_app;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+/**
+ * Created by sinhhx on 11/7/16.
+ */
+public class MovieTabFragment extends Fragment {
+
+    private int mPage;
+    private RecyclerView.LayoutManager mLayoutManager;
+    public static final String ARG_PAGE = "ARG_PAGE";
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView mRecyclerView;
+
+    public static MovieTabFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        MovieTabFragment fragment = new MovieTabFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        final View view = inflater.inflate(R.layout.movietabrecycler, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MovieDetailAdapter(getContext());
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        return view;
+
+
+
+
+
+    }
+}
+
