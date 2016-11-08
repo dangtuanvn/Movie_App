4fad4b230a1f93c54db3a783e7f797e820939391
dangtuanvn <dangtuan7112@gmail.com>
2016-11-08 17:07:34 +0700

    Add different in Update time and Current time calculation in NewsDeserializer.

--- app/src/main/java/com/example/dangtuanvn/movie_app/model/converter/NewsDeserializer.java
+++ app/src/main/java/com/example/dangtuanvn/movie_app/model/converter/NewsDeserializer.java
@@ -49,50 +49,52 @@ public class NewsDeserializer extends EasyDeserializer<News> {
             String dateUpdate = getStringValue(jsonObject.get("date_update"), null);
             if (dateUpdate != null) {
                 news.setDateUpdate(dateUpdate);
-
-                // Calculate difference in update time and current time
-                try {
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    String currentTime = sdf.format(new Date());
-                    String newsTime = dateUpdate;
-//                    Log.i("CURRENT TIME", currentTime + "");
-//                    Log.i("NEWS TIME", newsTime + "");
-
-                    SimpleDateFormat ymd = new SimpleDateFormat("yyyy-MM-dd");
-                    Date date1 = ymd.parse(currentTime);
-                    Date date2 = ymd.parse(newsTime);
-
-                    int numberOfDays = (int) ((date1.getTime() - date2.getTime()) / (3600 * 24 * 1000));
-
-                    if (numberOfDays > 30) {
-                        news.setTimeDifference(numberOfDays / 30 + " months");
-//                        Log.i("MONTH DIFFERENCE", numberOfDays / 30 + " months ago");
-                    } else if (numberOfDays > 0) {
-                        news.setTimeDifference(numberOfDays + " days");
-//                        Log.i("DAY DIFFERENCE", numberOfDays + " days ago");
-                    } else {
-                        SimpleDateFormat hms = new SimpleDateFormat("HH:mm:ss");
-                        String[] splitedTime1 = currentTime.split("\\s+");
-                        String[] splitedTime2 = newsTime.split("\\s+");
-                        Date time1 = hms.parse(splitedTime1[1]);
-                        Date time2 = hms.parse(splitedTime2[1]);
-                        long minuteDifference = (time1.getTime() - time2.getTime()) / 1000 / 60;
-
-                        if (minuteDifference > 60) {
-                            news.setTimeDifference(minuteDifference / 60 + " hours ago");
-//                            Log.i("HOUR DIFFERENCE", minuteDifference / 60 + " hours");
-                        } else {
-                            news.setTimeDifference(minuteDifference + " minutes ago");
-//                            Log.i("MINUTE DIFFERENCE", minuteDifference + " minutes");
-                        }
-                    }
-
-                } catch (ParseException e) {
-                    Log.i("TIME CALC EXCEPTION", e.getMessage());
-                }
-//                Log.i("BLANK", " ");
+                calculateTime(news, dateUpdate);
             }
         }
         return news;
+    }
+
+    public void calculateTime(News news, String dateUpdate){
+
+        // Calculate difference in update time and current time
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentTime = sdf.format(new Date());
+            String newsTime = dateUpdate;
+//                    Log.i("CURRENT TIME", currentTime + "");
+//                    Log.i("NEWS TIME", newsTime + "");
+
+            SimpleDateFormat ymd = new SimpleDateFormat("yyyy-MM-dd");
+            Date date1 = ymd.parse(currentTime);
+            Date date2 = ymd.parse(newsTime);
+
+            int numberOfDays = (int) ((date1.getTime() - date2.getTime()) / (3600 * 24 * 1000));
+
+            if (numberOfDays > 30) {
+                news.setTimeDifference(numberOfDays / 30 + " months");
+//                        Log.i("MONTH DIFFERENCE", numberOfDays / 30 + " months ago");
+            } else if (numberOfDays > 0) {
+                news.setTimeDifference(numberOfDays + " days");
+//                        Log.i("DAY DIFFERENCE", numberOfDays + " days ago");
+            } else {
+                SimpleDateFormat hms = new SimpleDateFormat("HH:mm:ss");
+                String[] splitedTime1 = currentTime.split("\\s+");
+                String[] splitedTime2 = newsTime.split("\\s+");
+                Date time1 = hms.parse(splitedTime1[1]);
+                Date time2 = hms.parse(splitedTime2[1]);
+                long minuteDifference = (time1.getTime() - time2.getTime()) / 1000 / 60;
+
+                if (minuteDifference > 60) {
+                    news.setTimeDifference(minuteDifference / 60 + " hours ago");
+//                            Log.i("HOUR DIFFERENCE", minuteDifference / 60 + " hours");
+                } else {
+                    news.setTimeDifference(minuteDifference + " minutes ago");
+//                            Log.i("MINUTE DIFFERENCE", minuteDifference + " minutes");
+                }
+            }
+        } catch (ParseException e) {
+            Log.i("TIME CALC EXCEPTION", e.getMessage());
+        }
     }
 }
