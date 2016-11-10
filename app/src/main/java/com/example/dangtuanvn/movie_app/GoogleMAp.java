84b4234856646b68faae53b3af27e09045cc0dfc
SinhHa <sinh.ha1992@gmail.com>
2016-11-10 13:23:16 +0700

    google map implement

---
+++ app/src/main/java/com/example/dangtuanvn/movie_app/GoogleMAp.java
@@ -0,0 +1,45 @@
+package com.example.dangtuanvn.movie_app;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by sinhhx on 11/10/16.
+ */
+public class GoogleMAp extends AppCompatActivity implements OnMapReadyCallback {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.googlemap);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(10.769095, 106.683169))
+                .title("Marker"));
+        CameraUpdate center=
+                CameraUpdateFactory.newLatLng(new LatLng(10.769095,
+                        106.683169));
+        CameraUpdate zoom= CameraUpdateFactory.zoomTo(15);
+
+        map.moveCamera(center);
+        map.animateCamera(zoom);
+
+    }
+
+}
