ad49959fa302c39d77de191b344e764f9b2e14da
dangtuanvn <dangtuan7112@gmail.com>
2016-11-24 11:53:18 +0700

    Fixing Merge mistake.

--- app/src/main/java/com/example/dangtuanvn/movie_app/fragment/CinemaTabFragment.java
+++ app/src/main/java/com/example/dangtuanvn/movie_app/fragment/CinemaTabFragment.java
@@ -1,9 +1,7 @@
 package com.example.dangtuanvn.movie_app.fragment;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
@@ -15,7 +13,6 @@ import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -218,11 +215,9 @@ public class CinemaTabFragment extends Fragment {
                     CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
 
                     map.moveCamera(center);
-
                     map.animateCamera(zoom);
 
                     List<Cinema> sortedCinemaList = selectionSort(cinemaList);
-
 
                     RecyclerView.Adapter adapter = new CinemaTabAdapter(getContext(), sortedCinemaList);
                     recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -286,8 +281,7 @@ public class CinemaTabFragment extends Fragment {
                     return false;
                 }
             }
-        })
-        ;
+        });
     }
 
     // Google Map functions
